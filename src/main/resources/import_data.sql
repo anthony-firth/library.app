53206f955b8efae764660bddb6eec16fde847d14
anthony-firth <anthony.firth@bigpond.com>
2015-07-17 22:20:07 +1000

    Updated data.

--- src/main/resources/import_data.sql
+++ src/main/resources/import_data.sql
@@ -7,8 +7,11 @@ INSERT INTO public.book (id, title, author, isbn, version) VALUES (1, 'Go Set a 
 INSERT INTO public.book (id, title, author, isbn, version) VALUES (2, 'Aussie Grit', 'Mark Webber', '3249537453457', 0);
 INSERT INTO public.book (id, title, author, isbn, version) VALUES (3, 'Enchanted Forest', 'Johanna Basford', '9457345344443', 0);
 INSERT INTO public.book (id, title, author, isbn, version) VALUES (4, 'Slow Cooker Central', 'Paulene Christie', '8436545634555', 0);
+INSERT INTO public.book (id, title, author, isbn, version) VALUES (5, 'The Goldfinch', 'Donna Tartt', '4385763456332', 0);
+
 
 INSERT INTO public.loan (id, personId, bookId, version) VALUES (1,1,4,0);
 INSERT INTO public.loan (id, personId, bookId, version) VALUES (2,2,1,0);
 INSERT INTO public.loan (id, personId, bookId, version) VALUES (3,3,2,0);
 INSERT INTO public.loan (id, personId, bookId, version) VALUES (4,4,3,0);
+INSERT INTO public.loan (id, personId, bookId, version) VALUES (5,1,5,0);
